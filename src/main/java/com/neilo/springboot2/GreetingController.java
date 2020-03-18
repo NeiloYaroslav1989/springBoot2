39e2fd5eb85ba8ceb74126ca5de075352ad768c0
NeiloYaroslav1989 <neiloyv@gmail.com>
2020-03-18 12:49:46 +0200

    Create project. MVC is working

---
+++ src/main/java/com/neilo/springboot2/GreetingController.java
@@ -0,0 +1,26 @@
+package com.neilo.springboot2;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping
+    public String main(Map<String, Object> model) {
+        model.put("some", "Hello, Yaroslav");
+        return "main";
+    }
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name,
+                           Map<String, Object> model) {
+        model.put("name", name);
+        return "greeting";
+    }
+
+}
